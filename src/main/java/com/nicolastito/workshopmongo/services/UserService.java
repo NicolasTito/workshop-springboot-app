e9ea1a3def55d07898632a6907d16a8956ae926b
Nicoalas Tito <mrnicolastito@icloud.com>
2020-04-04 18:18:03 +0100

    Find User by id

--- src/main/java/com/nicolastito/workshopmongo/services/UserService.java
+++ src/main/java/com/nicolastito/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.nicolastito.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nicolastito.workshopmongo.domain.User;
 import com.nicolastito.workshopmongo.repository.UserRepository;
+import com.nicolastito.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException(id));
 	}
 }
